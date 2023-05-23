c2ec71f2def19c4454614db350b090a3a70c1c52
Verdier <ldverdier@gmail.com>
2023-05-23 09:57:35 +0200

    feat: add test cases

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,8 +1,9 @@
 import { toArabic } from './index';
 
-test.each([{ romanNumber: 'I', expected: 1 }])(
-  'convert roman to arabic',
-  ({ romanNumber, expected }) => {
-    expect(toArabic(romanNumber)).toBe(expected);
-  },
-);
+test.each([
+  { romanNumber: 'I', expected: 1 },
+  { romanNumber: 'II', expected: 2 },
+  { romanNumber: 'III', expected: 3 },
+])('convert roman to arabic', ({ romanNumber, expected }) => {
+  expect(toArabic(romanNumber)).toBe(expected);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 export const toArabic = (romanNumber: string): number => {
-  return 1;
+  const ROMAN_TO_ARABIC: { [key: string]: number } = {
+    I: 1,
+  };
+
+  return romanNumber.split('').reduce((acc: number, letter: string) => {
+    return acc + ROMAN_TO_ARABIC[letter];
+  }, 0);
 };
