39fe65e6b4ee226fc926dacc0587e6d91ba43147
Verdier <ldverdier@gmail.com>
2023-06-02 17:36:27 +0200

    feat: implement first tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,28 @@
-import { add } from './index';
+import { addTwoRomanNumerals } from './index';
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(add(1, 2)).toBe(3);
+/**
+ * Add two roman numbers
+ *
+ * The main intent of this kata is to practice Mob Programming and agree on a preferred mobbing style for the team.
+ * For this we will run a variant of the Roman Numerals Converter kata, where we intend this time to calculate the sum of 2 roman numbers.
+ * To add a bit of fun, we will do it without any use of integers
+ *
+ * Examples:
+ *
+ * I + I = II
+ * IV + X = XIV
+ * XCIX + I = C
+ * CXXIV + MDCCCXCV = MMXIX
+ */
+
+test.each([
+  { a: 'I', b: 'I', result: 'II' },
+  { a: 'II', b: 'II', result: 'IV' },
+  { a: 'III', b: 'II', result: 'V' },
+  { a: 'V', b: 'I', result: 'VI' },
+  { a: 'V', b: 'IV', result: 'IX' },
+  { a: 'IV', b: 'X', result: 'XIV' },
+])('add numbers', ({ a, b, result }) => {
+  expect(addTwoRomanNumerals(a, b)).toBe(result);
+  expect(addTwoRomanNumerals(b, a)).toBe(result);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-export const add = (a: number, b: number) => a + b;
+/**
+ * I + I = II
+ * IV + X = XIV
+ * XCIX + I = C
+ * CXXIV + MDCCCXCV = MMXIX
+ */
+
+export const addTwoRomanNumerals = (a: string, b: string) => {
+  const translations: string[] = ['X', 'IV', 'I'];
+  const indexOfA = translations.indexOf(a);
+  const indexOfB = translations.indexOf(b);
+
+  let result = indexOfA < indexOfB ? a + b : b + a;
+
+  const hashmap = {
+    IIIII: 'V',
+    IIII: 'IV',
+    VIV: 'IX',
+  };
+
+  Object.entries(hashmap).forEach(([key, value]) => {
+    result = result.replace(key, value);
+  });
+
+  return result;
+};
