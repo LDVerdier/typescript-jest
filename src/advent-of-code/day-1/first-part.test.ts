177240b8d7ede4c7830ce776aa8ceee5ae659a81
Verdier <ldverdier@gmail.com>
2023-12-07 10:00:06 +0100

    test: second part first day

--- src/advent-of-code/day-1/first-part.test.ts
+++ src/advent-of-code/day-1/first-part.test.ts
@@ -28,3 +28,68 @@ describe('first part', () => {
     expect(numbers.reduce((a, b) => a + b)).toBe(57346);
   });
 });
+
+class NumberAtIndex {
+  private static NUMBERS_HASH_MAP = {
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+  };
+  constructor(private readonly value: string, private readonly index: number) {}
+
+  toIntString(): string {
+    if (this.value.length === 1) {
+      return this.value;
+    }
+
+    return NumberAtIndex.NUMBERS_HASH_MAP[
+      this.value as keyof typeof NumberAtIndex.NUMBERS_HASH_MAP
+    ].toString();
+  }
+}
+
+function matchOverlap(input: string, re: RegExp) {
+  const numbersAtIndex: NumberAtIndex[] = [];
+  let m;
+  // Prevent infinite loops
+  if (!re.global) re = new RegExp(re.source, (re + '').split('/').pop() + 'g');
+  while ((m = re.exec(input))) {
+    re.lastIndex -= m[0].length - 1;
+    numbersAtIndex.push(new NumberAtIndex(m[0] as string, re.lastIndex - 1));
+  }
+  return numbersAtIndex;
+}
+
+const extractFirstAndLastDigit2 = (line: string): number => {
+  const numbers = matchOverlap(
+    line,
+    /(one|two|three|four|five|six|seven|eight|nine|[0-9])/g,
+  ).map((numberAtIndex) => numberAtIndex.toIntString());
+
+  return +`${numbers[0]}${numbers[numbers.length - 1]}`;
+};
+
+describe('second part', () => {
+  it('should...', () => {
+    const result = Array.from(
+      matchOverlap(
+        'twone2six',
+        /(one|two|three|four|five|six|seven|eight|nine|[0-9])/g,
+      ),
+    ).map((numberAtIndex) => numberAtIndex.toIntString());
+    expect(result).toBe('2126');
+  });
+
+  it('final result', () => {
+    const lines = extractValuesFromFile(resolve(__dirname, 'values.txt'));
+    const numbers = lines.map(extractFirstAndLastDigit2);
+
+    expect(numbers.reduce((a, b) => a + b)).toBe(57345);
+  });
+});
